9743ac54dbcf680fcb76d803fcd63be7801bc976
presly <presly>
2017-12-13 18:58:20 +0530

    stock

--- js/service/httpService.js
+++ js/service/httpService.js
@@ -5,15 +5,16 @@ app.service('HttpService', function($resource, $alert, resourceErrorHandler, hos
         'getAllCategories':{url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/getAllCategories',method:'POST', interceptor : {responseError : resourceErrorHandler}},
         'saveProduct':   {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/saveProduct', method:'PUT', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
         'saveCategory':   {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/saveCategory', method:'PUT', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
-        'deleteCategory': {url :  host.get() +$rootScope.cartPort+'ebuy-cart-service/cart/deleteCategory/:id', method:'DELETE', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
-        'deleteSubCategory': {url :  host.get() +$rootScope.cartPort+'ebuy-cart-service/cart/deleteSubCategory/:id', method:'DELETE', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
+        'deleteCategory': {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/deleteCategory/:id', method:'DELETE', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
+        'deleteSubCategory': {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/deleteSubCategory/:id', method:'DELETE', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
         'getAllSubCategories':   {url : host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/getAllSubCategories', method:'POST', interceptor : { responseError : resourceErrorHandler}},
         'saveSubCategory':   {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/saveSubCategory', method:'PUT', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
         'getAllItems':   {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/getAllItems', method:'POST', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
         'saveItem':   {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/saveItem', method:'PUT', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
         'deleteItem': {url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/deleteItem/:id', method:'DELETE', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}},
         'getAllCustomer':{url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/customer/getAllCustomer',method:'POST', interceptor : {responseError : resourceErrorHandler}},
-        'getAllStockItems':{url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/getAllStockItems',method:'POST', interceptor : {responseError : resourceErrorHandler}}
+        'getAllStockItems':{url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/getAllStockItems',method:'POST', interceptor : {responseError : resourceErrorHandler}},
+        'saveStockItems':{url :  host.get() +$rootScope.cartPort+'/ebuy-cart-service/cart/saveStockItem',method:'PUT', interceptor : { response : resourceSuccessHandler ,responseError : resourceErrorHandler}}
     });
 });
 
